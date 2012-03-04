b6c64c13eb6e52c51ed08593392d3d944822d30c
gustavoguzr1@gmail.com <gustavoguzr1@gmail.com@2974037c-f23b-5115-c3fe-8e452b3902ec>
2012-03-04 19:57:19 +0000

    adding RST

---
+++ trunk/DesignDir/blocks/RTS/testbench/testbench.v
@@ -0,0 +1,8 @@
+//Testbench: RTS
+
+`timescale 1ns / 100ps
+
+module testbench;
+
+
+endmodule
---
+++ trunk/DesignDir/blocks/RTS/tool_info/runall.sh
@@ -0,0 +1,4 @@
+cd /home/gustavo/MDE/uP_Design/svnMIPS/trunk/DesignDir/blocks/RTS/tool_info/
+vlib.exe work
+vlog.exe -f /home/gustavo/MDE/uP_Design/svnMIPS/trunk/DesignDir/blocks/RTS/tool_info/files
+vsim.exe testbench -do do.tcl
