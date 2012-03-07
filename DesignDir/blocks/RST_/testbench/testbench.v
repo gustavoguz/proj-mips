640fb2fab32a0362002055dde25eae317bbc3471
gustavoguzr1@gmail.com <gustavoguzr1@gmail.com@2974037c-f23b-5115-c3fe-8e452b3902ec>
2012-03-07 03:58:20 +0000

    rst testbench v0

--- DesignDir/blocks/RST_/testbench/testbench.v
+++ DesignDir/blocks/RST_/testbench/testbench.v
@@ -3,6 +3,55 @@
 `timescale 1ns / 100ps
 
 module testbench;
+  	reg        	clock;
+  	reg        	reset;
 
+  	reg  	[ 4:0] 	Rsaddr_rst;
+  	wire 	[ 4:0] 	Rstag_rst;
+  	wire       	Rsvalid_rst;
+
+  	reg  	[ 4:0] 	Rtaddr_rst;
+  	wire 	[ 4:0] 	Rttag_rst;
+  	wire       	Rtvalid_rst;
+  
+  	reg  	[ 4:0] 	RB_tag_rst;
+	reg        	RB_valid_rst;
+
+ 	reg  	[ 4:0] 	Wdata_rst;
+  	reg  	[ 4:0] 	Waddr_rst;
+	reg 	[31:0] 	Wen0_rst;
+	reg 		Wen_rst;
+	wire 		Wen1_rst;
+
+rst rst (
+  	.clock(clock),
+  	.reset(reset),
+
+  	.Rsaddr_rst(Rsaddr_rst),
+  	.Rstag_rst(Rstag_rst),
+  	.Rsvalid_rst(Rsvalid_rst),
+  	
+	.Rttag_rst(Rttag_rst),
+  	.Rtvalid_rst(Rtvalid_rst),
+  
+  	.RB_tag_rst(RB_tag_rst),
+	.RB_valid_rst(RB_valid_rst),
+
+ 	.Wdata_rst(Wdata_rst),
+  	.Waddr_rst(Waddr_rst),
+	.Wen0_rst(Wen0_rst),
+	.Wen_rst(Wen_rst),
+	.Wen1_rst(Wen1_rst) 
+	);
+always begin 
+#5	clock=!clock;
+end
+
+initial  begin
+	clock = 0;
+	reset = 1;
+#10	reset = 0;
+#30	$finish;
+end
 
 endmodule
